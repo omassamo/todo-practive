fb48e2908ca131b6c8e5c8f310d3bc7f963e325a
Omassamo <samodenmark@gmail.com>
2019-08-05 16:27:33 +0200

    Added test for posting task to firebase with static values

--- public/addTask.js
+++ public/addTask.js
@@ -1,26 +1,49 @@
+// save 1 task with static content to firebase
+function addTask() {
+	
+	// // Get a reference to the database service
+	// var database = firebase.database();
 
-// Get a reference to the database service
-var database = firebase.database();
+	console.log('addTask');
 
-// get task list from firebase
-document.addEventListener("DOMContentLoaded", function(event) { 
- console.log("document ready");
-});
+	var data = JSON.stringify({
+	  "taskTest": {
+	    "status": true
+	  }
+	});
 
+	var xhr = new XMLHttpRequest();
+	xhr.withCredentials = true;
 
-// click the button = insert new row with cells.
-function addTask (){
+	xhr.addEventListener("readystatechange", function () {
+	  if (this.readyState === this.DONE) {
+	    console.log(this.responseText);
+	  }
+	});
 
-var table = document.getElementById("task-table"); 
+	xhr.open("POST", "https://to-do-list-4990e.firebaseio.com/tasks.json");
+	xhr.setRequestHeader("content-type", "application/json");
 
-var row = table.insertRow(1);
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1); 		
-
-cell1.innerHTML = '<input type ="text">' ;
-cell2.innerHTML = '<input type ="checkbox">'
+	xhr.send(data);
 
 };
 
-// save task list to firebase
+// // get task list from firebase - TO DO
+// document.addEventListener("DOMContentLoaded", function(event) { 
+//  console.log("document ready");
+// });
 
+
+// // click the button = insert new row with cells.
+// function addTask (){
+
+// var table = document.getElementById("task-table"); 
+
+// var row = table.insertRow(1);
+// var cell1 = row.insertCell(0);
+// var cell2 = row.insertCell(1); 		
+
+// cell1.innerHTML = '<input type ="text">' ;
+// cell2.innerHTML = '<input type ="checkbox">'
+
+// };
