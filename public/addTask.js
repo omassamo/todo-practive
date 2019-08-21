3aa4ffc79b0362a59b76d4f8ed095640965f05a8
Omassamo <samodenmark@gmail.com>
2019-08-21 14:40:38 +0200

    Clean up

--- public/addTask.js
+++ public/addTask.js
@@ -1,10 +1,16 @@
+// Path and JSON variables
+var mainPath = "/tasks/";
+var mainRef = "tasks";
+var name = "name";
+var status = "status";
+
 // wait for DOM to load and load tasks
 document.addEventListener("DOMContentLoaded", () => {
 	console.log("Ready");
 
 // Intitial -> Get tasks from firebase and update when child added - sorting is not really workingter
-// I should have event_listeners updating the list on child_removed and child_updated
-	var tasksJSON = firebase.database().ref('tasks').orderByChild('timestamp');
+// I should have event_listeners updating the list on child_removed and child_updated instead of using Javscript to update the DOM
+	var tasksJSON = firebase.database().ref(mainRef	).orderByChild('timestamp');
 	tasksJSON.on('child_added', function updateList (snapshot) {
 		var taskList = (snapshot.val());
 		taskKey = snapshot.key;
@@ -13,8 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			$("#task-table").append("<tr id='" + taskKey + "'><td>" + "<input oninput='updateName(this)' id='text-" + taskKey + "'value='" + taskList.name + "'>" + "</input>" + "</td>" + "<td><input onclick='updateStatus(this)'  id='status-" + taskKey + "' type='checkbox' checked></td><td><button onclick='deleteTask(this)'>❌</button></td></tr>");
 			} else {
 			$("#task-table").append("<tr id='" + taskKey + "'><td>" + "<input oninput='updateName(this)' id='text-" + taskKey + "'value='" + taskList.name + "'>" + "</input>" + "</td>" + "<td><input onclick='updateStatus(this)' id='status-" + taskKey + "' type='checkbox'></td><td><button onclick='deleteTask(this)'>❌</button></td></tr>");
-			};
-
+		};
 	});
 
 });
@@ -37,16 +42,15 @@ function addTask(name, status) {
 	console.log(postData);	
 
 	// Get a key for the task
-	var newTaskKey = firebase.database().ref().child('tasks').push().key;
+	var newTaskKey = firebase.database().ref().child(mainRef).push().key;
 	console.log(newTaskKey);
 
 	//define path to update
 	var updates = {};
-	updates['/tasks/' + newTaskKey] = postData;
+	updates[mainPath + newTaskKey] = postData;
 
 	//write the new taskdata
 	return firebase.database().ref().update(updates);
-
 };
 
 // Update status of task when user checks & unchecks checkbox - dirty trick with the id ✅
@@ -59,13 +63,8 @@ function updateStatus (status) {
 	// is the checkbox checked?
 	var x = document.getElementById(elementId).checked;
 	console.log(x);
-	// Define the update based on if checkbox is/ is not checked - should be possible to just add x to the JSON instead of having the if/ else statement
-	if (x == true) {
-		return firebase.database().ref('/tasks/' + parentId).update({"status": true});
-		} else {
-		return firebase.database().ref('/tasks/' + parentId).update({"status": false});
-	}; 
-
+	// update status in firebase
+	return firebase.database().ref(mainPath + parentId).update({status: x});
 };	
  
 // Update task name when user types a new name - dirty trick with the id ✅
@@ -78,8 +77,7 @@ function updateName (name) {
 	var taskName = document.getElementById(elementId).value;
 	
 	//update firebase with task name
-	return firebase.database().ref('/tasks/' + parentId).update({"name": taskName})
-
+	return firebase.database().ref(mainPath + parentId).update({name: taskName});
 };
 
 //delete task from firebase and remove table row - this is cheating as it'll only work if user has only 1 page of list open. Should be a proper firebase update✅
@@ -89,7 +87,7 @@ function deleteTask (task) {
 	// the id of the delete button
 	var parentElement = document.getElementById(parentId);
 	//remove the parent element
-	$(parentElement).remove();
+	parentElement.remove();
 	//remove the element from firebase
-	return firebase.database().ref('/tasks/' + parentId).remove();
+	return firebase.database().ref(mainPath + parentId).remove();
 };
